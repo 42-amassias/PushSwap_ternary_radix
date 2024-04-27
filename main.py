0843abd83de443c2b4ee688ef57828338e9f7f90
amassias <amassias@student.42lehavre.fr>
2024-04-27 18:32:02 +0200

    Added: now checking (& parsing) input validity.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
+import sys
 from push_swap import PushSwap
 
-if __name__ == "__main__":
-	ps = PushSwap[int]([1, 2, 3])
-	print(ps, end='\n\n')
+USE_ARGV = False
+DEFAULT_VALUES = [ 1, 2, 3 ]
 
-	ps.sa()
-	print(ps, end='\n\n')
+def	argv_to_values(argv: list[str]) -> list[int]:
+	return (list(map(int, sum(map(str.split, argv[1:]), []))))
+
+def	check_values(values : list[int]) -> bool:
+	l = len(values)
+	if l < 1:
+		print("There should be at least one value", file=sys.stderr)
+		return (False)
+	if l != len(set(values)):
+		print("There are duplicate values in your input", file=sys.stderr)
+		return (False)
+	return (True)
+
+if __name__ == "__main__":
+	values = argv_to_values(sys.argv) if USE_ARGV else DEFAULT_VALUES
+	if not check_values(values):
+		exit(1)
+	ps = PushSwap[int](values)
