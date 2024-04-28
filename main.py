843551b3a032f293c02d2e39dfcfd93bb23c499b
amassias <amassias@student.42lehavre.fr>
2024-04-28 17:28:47 +0200

    Fix: Crash when using argv (type conversion).

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ USE_ARGV: bool = False
 DEFAULT_VALUES: List[Ternary] = list(map(Ternary, [ 8, 4, 3, 6, 1, 2, 0, 7, 5 ]))
 
 def	argv_to_values(argv: List[str]) -> List[Ternary]:
-	return (list(map(Ternary, sum(map(str.split, argv[1:]), []))))
+	return (list(map(Ternary, map(int, sum(map(str.split, argv[1:]), [])))))
 
 def	check_values(values : List[Ternary]) -> bool:
 	if not values:
